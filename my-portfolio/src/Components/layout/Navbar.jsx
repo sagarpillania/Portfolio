a53ae6de88e1ba9ff81dd690a2176b78785fd81e
Sagar Pillania <121743997+sagarpillania@users.noreply.github.com>
2026-07-03 23:47:08 +0530

    kuch bhi

---
+++ my-portfolio/src/Components/layout/Navbar.jsx
@@ -0,0 +1,22 @@
+
+function Navbar(){
+
+    return(
+        <>
+        <div className="navbar">
+            <div className="logo">Logo</div>
+            <div className="sidebar">
+                <h2>Home</h2>
+                <h2>Services</h2>
+                <h2>About</h2>
+                <h2>Skills</h2>
+                <h2>Projects</h2>
+            </div>
+            <div>Let's Talk</div>
+        </div>
+        </>
+    )
+
+}
+
+export default Navbar;
