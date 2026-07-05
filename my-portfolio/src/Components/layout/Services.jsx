462c000dfd384bb8027f645a3cda20a8dbb5d146
Sagar Pillania <121743997+sagarpillania@users.noreply.github.com>
2026-07-05 23:47:58 +0530

    ese hi

---
+++ my-portfolio/src/Components/layout/Services.jsx
@@ -0,0 +1,10 @@
+
+function Services(){
+
+    return(
+        <>
+        </>
+    )
+}
+
+export default Services;
---
+++ my-portfolio/src/Components/ui/Cards.jsx
@@ -0,0 +1,17 @@
+
+
+function Cards(){
+
+    return(
+        <>
+        <div>
+            <h2>UI/ux Website Design</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus ipsam officiis ea qui expedita accusantium distinctio</p>
+            <div className="sidebar flex justify-center items-center gap-6 border border-zinc-800 py-2 px-6 rounded-full bg-[#121212]/80 backdrop-blur-md text-sm text-zinc-400"></div>
+        </div>
+        
+        </>
+    )
+}
+
+export default Cards;
